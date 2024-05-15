f62e6b3afa20be88e1c6a8a8d1745452894da7a7
alex@tuf <alex@tuf>
2024-05-16 07:28:05 +0800

    feat: get mongosdb uri from .env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { MongoClient } = require("mongodb");
 
 async function run() {
@@ -6,12 +7,13 @@ async function run() {
   // Altas cluster specifics. Be sure it includes
   // a valid username and password! Note that in a production environment,
   // you do not want to store your password in plain-text here.
-  const uri =
-    "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+  //const uri = process.env.MONGOSDB_URI;
+  //  "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
 
   // The MongoClient is the object that references the connection to our
   // datastore (Atlas, for example)
-  const client = new MongoClient(uri);
+  console.log("MONGOSDB_URI", process.env);
+  const client = new MongoClient(process.env.MONGOSDB_URI);
 
   // The connect() method does not attempt a connection; instead it instructs
   // the driver to connect using the settings provided when a connection
